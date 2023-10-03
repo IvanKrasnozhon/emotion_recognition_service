9a3d448977ad074708de08c1254f1cba9c4d9a71
Ivan Krasnozhon <ivan0505000267@gmail.com>
2023-10-03 23:12:39 +0300

    conversion to 5 emotions(angry, happy, concerned, stare, neutral)

--- main.py
+++ main.py
@@ -6,6 +6,37 @@ tokenizer = RobertaTokenizerFast.from_pretrained("arpanghoshal/EmoRoBERTa")
 model = TFRobertaForSequenceClassification.from_pretrained("arpanghoshal/EmoRoBERTa")
 emotion = pipeline('sentiment-analysis', model='arpanghoshal/EmoRoBERTa')
 
+emotion_groups = {
+    'Love': 'Happy',
+    'Admiration': 'Happy',
+    'Joy': 'Happy',
+    'Approval': 'Happy',
+    'Caring': 'Concerned',
+    'Excitement': 'Happy',
+    'Amusement': 'Happy',
+    'Gratitude': 'Happy',
+    'Desire': 'Happy',
+    'Anger': 'Angry',
+    'Optimism': 'Happy',
+    'Disapproval': 'Angry',
+    'Grief': 'Sadness',
+    'Annoyance': 'Angry',
+    'Pride': 'Happy',
+    'Curiosity': 'Concerned',
+    'Neutral': 'Neutral',
+    'Disgust': 'Angry',
+    'Disappointment': 'Angry',
+    'Realization': 'Stare',
+    'Fear': 'Concerned',
+    'Relief': 'Happy',
+    'Confusion': 'Concerned',
+    'Remorse': 'Concerned',
+    'Embarrassment': 'Concerned',
+    'Surprise': 'Stare',
+    'Sadness': 'Sadness',
+    'Nervousness': 'Concerned'
+}
+
 class EmotionAnalysisHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
@@ -18,8 +49,10 @@ class EmotionAnalysisHandler(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'application/json')
         self.end_headers()
 
+        print(emotion_labels[0]['label'])
+
         response_data = {
-            "emotion_labels": emotion_labels
+            "emotion_label": emotion_groups[str.capitalize(emotion_labels[0]['label'])]
         }
 
         self.wfile.write(json.dumps(response_data).encode('utf-8'))
